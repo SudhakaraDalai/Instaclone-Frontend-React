511d429256e338ad678e1f06949b0a0690208908
SudhakaraDalai <sudhakaradalai5@gmail.com>
2022-12-13 23:13:18 +0530

    React Post-View Page

---
+++ src/components/post-view/post-view.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import Card from "../../card";
+import Header from "../header/header";
+import axios from 'axios';
+
+const PostView = () =>{
+    
+    const [posts,setPosts] = useState([]);
+
+    useEffect(()=>{
+        fetch("http://localhost:3004/user").then((res)=>res.json()).then((data)=>{
+            setPosts(data);
+        }).catch((err)=>{
+            if(err){
+                console.log(err)
+            }
+        });
+        axios.get('https://sudhakar-instaclone-api.onrender.com')
+        .then(function (response) {
+            setPosts(response.data.reverse());
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    },[])
+    
+    return(
+       
+        <>
+            <Header />
+            <div className="post-container">
+            {posts.map((post,i)=>{
+                return (
+                    <Card post = {post} key = {i} />
+                )
+            })}
+        </div>
+        </>
+    )
+}
+
+export default PostView;
